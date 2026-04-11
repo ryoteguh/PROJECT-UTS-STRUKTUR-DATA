179be36da52596f7a74de1e47e0e37f1df548321
akssjjs0209nue <n9ak5q@gmail.com>
2026-04-11 09:32:28 +0800

    menambahkan fungsi peek dan display ke QueueLinkedList dan memberikan tampilan menu utama

--- UTS Struktur data.py
+++ UTS Struktur data.py
@@ -25,3 +25,29 @@ class Queuelinkedlist:
         if self.front is None:
             self.rear = None
         return temp.nama, temp.keluhan
+#Andi Pratama  NIM 2501010092 - Kelas C 
+#Membuat fungsi peek dan tampilkan untuk melihat data(antrian) serta membuat tampilan menu utama
+    def peek(self):
+        if self.front is None:
+            return None
+        return self.front.nama, self.front.keluhan
+    def tampilkan(self):
+        if self.front is None:
+            print("Queue Kosong")
+            return
+        current = self.front
+        no = 1
+        print(f"{'No' :<5} {'Nama':<20} {'keluhan'}")
+        print("-"*40)
+        while current:
+            print(f"{no:<5} {current.nama:<21}{current.keluhan}")
+            current = current.next
+            no += 1  
+qll = Queuelinkedlist()
+while True:
+    print("\n=== MENU ANTRIAN ===")
+    print("1. Tambah antrian (Enqueue)")
+    print("2. Keluarkan antrian (Dequeue)")
+    print("3. Lihat antrian terdepan (Peek)")
+    print("4. Tampilkan semua antrian")
+    print("5. Keluar")
